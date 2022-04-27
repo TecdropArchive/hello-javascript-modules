7d4312e03d1e82cc05ad08c149ac4132d6e6f5e3
devliber <dev@devliber.com>
2022-04-27 22:17:15 +0300

    First commit (existing source code)

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+/*
+ *
+ *  Hello JavaScript Modules
+ *  Copyright (c) 2018-2022 Tecdrop. All rights reserved.
+ *  Licensed under the MIT license.
+ *  https://www.tecdrop.com
+ *
+ */
+
+import helloJavaScriptModules from "/scripts/hello.js";
+
+helloJavaScriptModules();
